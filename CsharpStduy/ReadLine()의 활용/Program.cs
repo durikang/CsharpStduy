15ddcd9d6f2b9c7355bcb49f9b1be86a58460648
durikang <kooda21@naver.com>
2019-03-16 12:33:41 +0900

    업데이트

---
+++ CsharpStduy/ReadLine()의 활용/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadLine__의_활용
+{
+
+    class Program
+    {
+        struct Grade
+        {
+            public int kor, eng, math, total;
+            public float average;
+            
+        };
+        
+
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("성적 처리를 위한 학생 수를 입력해 주세요 :");
+            int nCount = int.Parse(Console.ReadLine());
+            Console.WriteLine("국 영 수 순으로 입력해 주세요");
+            Grade[] grade = new Grade[nCount];
+            FileStream fs = new FileStream("Student's Grade.txt",FileMode.Create);
+
+            for(int i = 0; i < nCount; i++)
+            {
+                grade[i].kor = int.Parse(Console.ReadLine());
+                grade[i].eng = int.Parse(Console.ReadLine());
+                grade[i].math = int.Parse(Console.ReadLine());
+                grade[i].total += grade[i].kor + grade[i].eng + grade[i].math;
+                grade[i].average = grade[i].total / 3;
+            }
+
+            using (StreamWriter sw =new StreamWriter(fs))
+            {
+
+            }
+
+
+        }
+    }
+}
---
+++ CsharpStduy/string 데이터 분리/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_데이터_분리
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.splt의 사용
+            var str = "국어: 90 영어: 100 수학: 70";
+            string[] str_Element = str.Split(new char[] { ' ' });//' '을 기준으로 문자열을 나누겠다.
+            var kor = int.Parse(str_Element[1]);
+            var eng = int.Parse(str_Element[3]);
+            var math = int.Parse(str_Element[5]);
+            var total = kor + eng + math;
+            var average = total / 3.0f;
+
+
+            Console.WriteLine($"국어: {kor}영어: {eng}수학: {math}총점: {total}평균: {Math.Round(average)}");
+
+
+        }
+    }
+}
